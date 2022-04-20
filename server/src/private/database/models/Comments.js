35252f43e4cf4f10d95a7ecf5d4d7f2dae63aff4
Carlos <cpellico@serbatic.es>
2022-04-20 16:20:13 +0200

    modelo comentarios

---
+++ server/src/private/database/models/Comments.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const comments = new mongoose.Schema({
+    user_id:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+        required: true
+    },
+    date_at:{
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+    comment:{
+        type: String,
+        required: true
+    },
+    product_id:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "products",
+        required: true
+    },
+    rating:{
+        rate:{
+            type: Number,
+            required: true,
+            default: 0
+        },
+        votes:{
+            type: Number,
+            required: true,
+            default: 0
+        }
+    }
+});
+
+export default mongoose.model("comments", comments);
